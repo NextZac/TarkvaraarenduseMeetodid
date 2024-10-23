9fec5677aa46fd74dde89457ef3ff66f15075d7f
NextZac <48414018+NextZac@users.noreply.github.com>
2024-10-23 12:39:52 +0300

    Another fix for the merge

--- frontend/src/views/Feedback.vue
+++ frontend/src/views/Feedback.vue
@@ -1,7 +1,7 @@
 <script setup>
-import BallSystem from '../components/BallSystem.vue'
-import InputSystem from '../components/InputSystem.vue';
-import Button from '../components/Button.vue';
+import BallSystem from "../components/BallSystem.vue";
+import InputSystem from "../components/InputSystem.vue";
+import Button from "../components/Button.vue";
 import BallSystem from "../components/BallSystem.vue";
 import InputSystem from "../components/InputSystem.vue";
 import Button from "../components/Button.vue";
@@ -12,53 +12,57 @@ import { watch } from "vue";
 
 const form = ref(null);
 
-const questions = ref({
-    id: 1,
-    question: 'Mata',
-},
-{
-    id: 2,
-    question: 'Mata',
-},
-{
-    id: 3,
-    question: 'Mata',
-},
-{
-    id: 4,
-    question: 'Mata',
-},
-{
-    id: 5,
-    question: 'Mata',
-});
+const questions = ref(
+    {
+        id: 1,
+        question: "Mata",
+    },
+    {
+        id: 2,
+        question: "Mata",
+    },
+    {
+        id: 3,
+        question: "Mata",
+    },
+    {
+        id: 4,
+        question: "Mata",
+    },
+    {
+        id: 5,
+        question: "Mata",
+    },
+);
 
-function FormValitation(questions){
+function FormValitation(questions) {
     let response = [];
 
     questions.forEach((question) => {
-        let ballsContainer = document.getElementById(question.id + 'a');
+        let ballsContainer = document.getElementById(question.id + "a");
 
         try {
-            let ball = ballsContainer.querySelector("[type='button'][aria-checked='true']");
+            let ball = ballsContainer.querySelector(
+                "[type='button'][aria-checked='true']",
+            );
 
-            let answer = ball.getAttribute('id');
+            let answer = ball.getAttribute("id");
 
             response.push({
                 id: question.id,
                 question: question.question,
-                answer: answer
+                answer: answer,
             });
         } catch (error) {
             console.log(error);
         }
     });
 
-    let textField = document.getElementById('textField');
+    let textField = document.getElementById("textField");
 
     response.push({
-        textField: textField
-    })
+        textField: textField,
+    });
 
     console.log(response);
 }
@@ -91,20 +95,14 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-col gap-8 bg-white w-full h-full rounded-[10px] p-6 sm:p-16">
     <div
         class="flex flex-col gap-8 bg-white w-full h-full rounded-[10px] p-6 sm:p-16"
     >
         <p class="font-bold text-[32px]">Feedback</p>
-
-        <BallSystem>How would you rate your experience?</BallSystem>
-        <BallSystem>How would you rate your experience?</BallSystem>
-        <BallSystem>How would you rate your experience?</BallSystem>
-        <BallSystem>How would you rate your experience?</BallSystem>
-        <BallSystem v-for="question in questions">{{
+        <BallSystem v-for="question in questions" :id="question.id + 'a'">{{
             question.question
         }}</BallSystem>
-        <InputSystem>Your thoughts</InputSystem>
-        <Button>Submit feedback</Button>
+        <InputSystem id="textField">Your thoughts</InputSystem>
+        <Button @click="FormValitation(questions)">Submit feedback</Button>
     </div>
 </template>
