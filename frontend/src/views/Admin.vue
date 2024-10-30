00029a12975315e9d44151bb0b1a4ebb3c671427
Kristofer Jürgenstein <50505081413@tptlive.ee>
2024-10-30 14:12:33 +0200

    Working on admin view frontend

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,18 +1,3 @@
-<template>
-    <div>
-        <h1>Answers</h1>
-        <ul>
-            <li v-for="(item, index) in answers" :key="index">
-                <strong>Question:</strong> {{ item.question || item.textField
-                }}<br />
-                <strong>Answer:</strong>
-                {{ item.answer || "No answer provided" }}
-                <br />
-            </li>
-        </ul>
-    </div>
-</template>
-
 <script setup>
 import { ref, onMounted } from "vue";
 
@@ -30,3 +15,38 @@ const fetchAnswers = async () => {
 };
 onMounted(fetchAnswers);
 </script>
+
+<template>
+    <div>
+        <h1>Answers</h1>
+        <ul>
+            <li v-for="(item, index) in answers" :key="index">
+                <strong>Question:</strong> {{ item.question || item.textField
+                }}<br />
+                <strong>Answer:</strong>
+                {{ item.answer || "No answer provided" }}
+                <br />
+            </li>
+        </ul>
+    </div>
+
+    <div class="flex flex-col w-full h-full gap-8">
+        <h1 class="text-3xl font-bold">Answers</h1>
+        <div class="flex flex-col gap-8 w-full h-full border-t border-[#B8B8B8] pt-8">
+
+            <div v-for="(item, index) in answers" class="flex flex-col w-full h-min bg-[#d1d1d1] rounded-lg p-6">
+                <h1 class="text-2xl font-bold border-b border-[#B8B8B8] pb-2 mb-2"> Form ID: {{ item.question }} </h1>
+                <span class="flex flex-col gap-6">
+                    <div class="flex flex-col gap-2 w-full h-full">
+                        <h1 class="text-xl font-bold">Question: {{ item.question }}</h1>
+                        <h1 class="text-xl font-bold">Answer: {{ item.answer }}</h1>
+                    </div>
+                    <div class="flex flex-col gap-2 w-full h-full">
+                        <h1 class="text-xl font-bold">Question: {{  }}</h1>
+                        <h1 class="text-xl font-bold">Answer: {{  }}</h1>
+                    </div>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
