4ef45f0e970b518b7128fea709235b72c63f8980
Kristofer Jürgenstein <50505081413@tptlive.ee>
2024-11-13 13:04:43 +0200

    Admin view done

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-const answers = ref([]);
+const answers = ref([{ "question": { "id": 1 }, "answer": "Dont know" }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "5" }, { "question": "Why are you here?", "answer": "5" }, {}] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "5" }, { "question": "Why are you here?", "answer": "5" }, { "textField": "TETETETETETETETETE" }] }, { "question": { "id": 1 }, "answer": [{ "textField": "asd" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }, { "textField": "sadwas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }, { "textField": "sadwas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }, { "textField": "sadwas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }, { "textField": "sadwas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "4" }, { "question": "Why are you here?", "answer": "4" }, { "textField": "sadwas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }] }, { "question": { "id": 1 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "2" }] }, { "question": { "id": 2 }, "answer": "Dont know" }, { "question": { "id": 3 }, "answer": "Dont know" }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "1" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "1" }, { "textField": "wawas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "2" }, { "question": "Who are you?", "answer": "1" }, { "textField": "nigg" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "2" }, { "question": "Who are you?", "answer": "1" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "2" }, { "question": "Who are you?", "answer": "4" }, { "textField": "nigga" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdaas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "2" }, { "question": "Why are you here?", "answer": "3" }, { "question": "Who are you?", "answer": "2" }, { "textField": "asdas" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "1" }, { "question": "Why are you here?", "answer": "2" }, { "question": "Who are you?", "answer": "2" }, { "textField": "nigg" }] }, { "question": { "id": 3 }, "answer": [{ "question": "Why do birds fly?", "answer": "4" }, { "question": "Why are you here?", "answer": "2" }, { "question": "Who are you?", "answer": "3" }, { "textField": "niggaaa" }] }]);
 
 const fetchAnswers = async () => {
     try {
@@ -13,38 +13,43 @@ const fetchAnswers = async () => {
         console.error("Error fetching answers:", error);
     }
 };
+
+const hidden = ref(false);
+const sorted = ref(false);
+const sortText = ref('Oldest -> Newest');
+
+const sortNewestToOldest = () => {
+    answers.value = [...answers.value].reverse();
+    sorted.value = !sorted.value;
+    if (sorted.value) sortText.value = 'Newest -> Oldest';
+    else sortText.value = 'Oldest -> Newest';
+};
+
 onMounted(fetchAnswers);
 </script>
 
 <template>
-    <div>
-        <h1>Answers</h1>
-        <ul>
-            <li v-for="(item, index) in answers" :key="index">
-                <strong>Question:</strong> {{ item.question || item.textField
-                }}<br />
-                <strong>Answer:</strong>
-                {{ item.answer || "No answer provided" }}
-                <br />
-            </li>
-        </ul>
-    </div>
-
     <div class="flex flex-col w-full h-full gap-8">
-        <h1 class="text-3xl font-bold">Answers</h1>
+        <span class="flex justify-center gap-4 items-center">
+            <h1 class="text-3xl font-bold">Answers</h1>
+            <button class="p-4 rounded-lg bg-[#d1d1d1] hover:bg-[#acacac]" @click="sortNewestToOldest">Current sort: {{ sortText }}</button>
+            <button class="p-4 rounded-lg bg-[#d1d1d1] hover:bg-[#acacac]" @click="hidden = !hidden">Hide "Dont know" answers</button>
+        </span>
         <div class="flex flex-col gap-8 w-full h-full border-t border-[#B8B8B8] pt-8">
 
-            <div v-for="(item, index) in answers" class="flex flex-col w-full h-min bg-[#d1d1d1] rounded-lg p-6">
-                <h1 class="text-2xl font-bold border-b border-[#B8B8B8] pb-2 mb-2"> Form ID: {{ item.question }} </h1>
+            <div v-for="(item, index) in answers" class="flex flex-col w-full h-min bg-[#d1d1d1] rounded-lg p-6"
+                :class="{ 'border border-[#da325c] !bg-[#da325c]/30 text-[#da325c]': typeof item.answer == 'string', 'hidden': typeof item.answer == 'string' && hidden }">
+                <h1 class="text-2xl font-bold border-b border-[#B8B8B8] pb-2 mb-2"
+                    :class="{ 'border-[#da325c]': typeof item.answer == 'string' }"> Form ID: {{ item.question.id }}
+                </h1>
                 <span class="flex flex-col gap-6">
-                    <div class="flex flex-col gap-2 w-full h-full">
-                        <h1 class="text-xl font-bold">Question: {{ item.question }}</h1>
-                        <h1 class="text-xl font-bold">Answer: {{ item.answer }}</h1>
+                    <div v-if="Array.isArray(item.answer)" v-for="(answer, index) in item.answer"
+                        class="flex flex-col gap-2 w-full h-full">
+                        <h1 v-if="answer.question" class="text-xl font-bold">Question: {{ answer.question }}</h1>
+                        <h1 v-if="answer.answer" class="text-xl font-bold">Answer: {{ answer.answer }}</h1>
+                        <h1 v-if="answer.textField" class="text-xl font-bold">Text Input: {{ answer.textField }}</h1>
                     </div>
-                    <div class="flex flex-col gap-2 w-full h-full">
-                        <h1 class="text-xl font-bold">Question: {{  }}</h1>
-                        <h1 class="text-xl font-bold">Answer: {{  }}</h1>
-                    </div>
+                    <h1 v-if="typeof item.answer == 'string'" class="text-xl font-bold">Answer: ?</h1>
                 </span>
             </div>
         </div>
